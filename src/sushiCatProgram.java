08f6a42139fd8ca3097aab8c1e6335c6a5bf48ee
Edson <edc5fc@gmail.com>
2022-06-03 22:19:09 -0700

    string concat

--- src/sushiCatProgram.java
+++ src/sushiCatProgram.java
@@ -1,9 +1,15 @@
+// import statements are here
 
+
+// this is the sushiCatProgram class
 public class sushiCatProgram {
-
+	
+	// this is the main method
 	public static void main(String[] args) {
-		System.out.println("something different!"); 
-
+		
+		String cat = "meoow purr lick meeoww cute UWU uwu :3 ;3 -3- o3o >3<";
+		String kitty = cat + " imma EVILL J KITTYZ";
+		System.out.println(kitty);
 	}
 
 }
