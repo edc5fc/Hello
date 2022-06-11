a7f28b011b51d11775473d758ec9aa0f161c4745
Edson <edc5fc@gmail.com>
2022-06-10 19:54:34 -0700

    deleted everything and started fresh again

--- src/sushiCatProgram.java
+++ src/sushiCatProgram.java
@@ -1,5 +1,4 @@
 // import statements are here
-import java.util.Scanner;
 
 // this is the sushiCatProgram class
 public class sushiCatProgram {
@@ -7,10 +6,6 @@ public class sushiCatProgram {
 	// this is the main method
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
-		int meowInput = scanner.nextInt(15); 
-		
-		System.out.println(meowInput);
 	}
 
 }
