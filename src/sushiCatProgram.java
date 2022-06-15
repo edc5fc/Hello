3c69aa55905fac78a6a7e8e4c7ce35c78d77d761
Edson <edc5fc@gmail.com>
2022-06-14 20:07:59 -0700

    made a silly password game

--- src/sushiCatProgram.java
+++ src/sushiCatProgram.java
@@ -1,11 +1,19 @@
 // import statements are here
+import java.util.Scanner;
 
 // this is the sushiCatProgram class
 public class sushiCatProgram {
 	
 	// this is the main method
 	public static void main(String[] args) {
+		String password = "thing.DONTGUESS(LET. ME! INNN!) correct.";
+		
+		System.out.println("Guess the password:");
+		
+		Scanner scanner = new Scanner(System.in);
+		String Guess  = scanner.nextLine();
+		
+		System.out.println(Guess.equals(password));
 		
 	}
-
 }
