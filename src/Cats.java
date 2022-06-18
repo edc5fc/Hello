6454c9ded4d03070742363d7208129576230fed3
Edson <edc5fc@gmail.com>
2022-06-17 20:15:07 -0700

    made Cats class and made a little cats game

---
+++ src/Cats.java
@@ -0,0 +1,17 @@
+//this is the cats class
+public class Cats {
+	
+	//properties
+	public long numberOfCats = 906_678_908_678L;
+	public String wantedCat;
+	public String catNameFirst;
+	public String catNameLast;
+	
+	//fields
+	String favoriteCatTreat;
+	
+	//methods
+	public String getCatName() {
+		return catNameFirst + " " + catNameLast;
+	};
+}
--- src/sushiCatProgram.java
+++ src/sushiCatProgram.java
@@ -1,19 +1,50 @@
-// import statements are here
+//import statements are here
 import java.util.Scanner;
 
-// this is the sushiCatProgram class
+//this is the sushiCatProgram class
 public class sushiCatProgram {
 	
-	// this is the main method
+	//this is the main method
 	public static void main(String[] args) {
-		String password = "thing.DONTGUESS(LET. ME! INNN!) correct.";
 		
-		System.out.println("Guess the password:");
+		//this is an instance of cats
+		Cats cat = new Cats();
 		
+		//this checks the cat population
+		System.out.println("cat population notification:");
+		if (cat.numberOfCats > 100_000_000F && cat.numberOfCats < 100_000_000_000_000F) {
+			System.out.println("The cat population is healthy. \n");
+		}
+		else if (cat.numberOfCats < 100_000_000F) 
+		{
+			System.out.println("ALERT! ALERT! The cat population is TOO SMALL!");
+		}
+		else 
+		{
+			System.out.println("ALERT! ALERT! The cat population is TOO LARGE!");
+		}
+		
+		//asks the user whether they like cats or dogs		
 		Scanner scanner = new Scanner(System.in);
-		String Guess  = scanner.nextLine();
+		String preferredAnimal = "";
 		
-		System.out.println(Guess.equals(password));
+		
+		while (true) {
+			System.out.println("Cats or dogs?");
+			preferredAnimal = scanner.nextLine();
+			
+			if (preferredAnimal.toLowerCase().equals("dogs")) {
+				System.out.println("you like dogs.");
+				break;
+			}
+			else if (preferredAnimal.toLowerCase().equals("cats"))
+			{
+				System.out.println("I like cats also!");
+				break;
+			}
+			else {System.out.println("What???");}
+		};
+		
 		
 	}
 }
