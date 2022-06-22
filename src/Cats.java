cfdf52bf0d7469613fdeca0b1f276461e6ddccd0
Edson <edc5fc@gmail.com>
2022-06-21 20:00:32 -0700

    Updated cat game and cats.class

--- src/Cats.java
+++ src/Cats.java
@@ -6,9 +6,15 @@ public class Cats {
 	public String wantedCat;
 	public String catNameFirst;
 	public String catNameLast;
+	public String[] newUsers;
+	public String[] tagExperts;
 	
 	//fields
 	String favoriteCatTreat;
+	int reactorNumber;
+	double packageCatDecimal;
+	String secretTextCat;
+	String thingyCode;
 	
 	//methods
 	public String getCatName() {
--- src/sushiCatProgram.java
+++ src/sushiCatProgram.java
@@ -45,6 +45,52 @@ public class sushiCatProgram {
 			else {System.out.println("What???");}
 		};
 		
+		//asks the name of the user
+		System.out.println("\nWhat's your username?");
+		String name = scanner.nextLine();
 		
+		switch(name)
+		{
+		default:
+			System.out.println("Hello, new user! sign in at Cats.javcom to continue. ;)");
+			break;
+		
+		case "CCat":
+			System.out.println("Hello, CCat. There is one issue for you: \nFix the caloneator.");
+			break;
+		
+		case "catty_impostor":
+			System.out.println("INTURDER ALERT!!!!! INTRUDER ALERT!!!! CATTY_IMPOSTOR IS HERE!!!");
+			break;
+			
+		case "Enamel-kitty":
+			System.out.println("Hello, Enamel-kitty. There are two issues for you: \n#1. Brush somebody's teeth. \n#2. Make catty_impostor eat junk food.");
+			break;
+		
+		case "neotech-advocatty":
+			System.out.println("Hello, neotech-advocatty! Please check everything to make sure that all the cats are good :)");
+			break;
+			
+		case "LC00duhh":
+		case "REALLY-LAZY-KITAZY":
+		case "lazzyCat":
+		case "lazy-cat":
+			System.out.println("PLEASE be less lazy.");
+			break;
+			
+		case "Amongus":
+		case "Rickroll":
+		case "Joe-mama":
+		case "grillbaby":
+			System.out.println("GO AWAY MEMES!!!!!!!");
+			break;
+		}
+		
+		//asks the user to say something random
+		System.out.println("\nSay something random: ");
+		String randomText = scanner.nextLine();
+		
+		String reaction = randomText.contains("chip kingdom") ? "NO CHIPS!!!":"Thanks for the random text!!!";
+		System.out.println(reaction);
 	}
 }
