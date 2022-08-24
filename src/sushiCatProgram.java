993438484dcfce8a27267d338d71748c31d84aa5
Edson <edc5fc@gmail.com>
2022-08-23 19:21:15 -0700

    added a switch statement to the program

--- src/sushiCatProgram.java
+++ src/sushiCatProgram.java
@@ -5,11 +5,26 @@ public class sushiCatProgram {
 	
 	//this is the main method
 	public static void main(String[] args) {
-		Mushroom m = new Mushroom();
-		m.toxicity = m.toxicity.apocalypticDeath;
-		int foodLevel = m.eat();
-		System.out.println(foodLevel);
-		System.out.println(m.toxicity);
-	}
+			Mushroom m = new Mushroom();
+			m.toxicity = m.toxicity.toxic;
+			int foodLevel = m.eat();
+
+			switch(m.toxicity) {
+				
+			case apocalypticDeath, certainDeath:
+				System.out.println("Do not eat if you don't want to die");
+				break;
+			
+			case veryToxic, toxic:
+				System.out.println("Do not eat if you don't want to have stomach pain");
+				break;
+			
+			case notToxic:
+				System.out.println("Ok to eat");
+			
+			}
+	
+		}
+	
 }
 
